ecce376941f5752246d62b96213831b5f6807efa
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-08-29 16:15:57 -0500

    prueba de paises

--- Server/src/Routes/index.js
+++ Server/src/Routes/index.js
@@ -1,5 +1,8 @@
 const { Router } = require("express");
+const { getAccessApi } = require("../Controller/CountriesController");
 
 const router = Router();
+
+router.get("/countries", getAccessApi);
 
 module.exports = router;
