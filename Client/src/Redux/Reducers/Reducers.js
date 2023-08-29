fff3e832faf5872a7ea37382fac1eb124441f1cc
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-08-29 22:58:05 +0200

    Configuracion de Redux
    Se ha configurado la Store para empezar a utilizar Redux.

---
+++ Client/src/Redux/Reducers/Reducers.js
@@ -0,0 +1,10 @@
+const initialState = {
+    algo: []
+}
+
+export default function rootReducer(state = initialState, action){
+    switch (action.type){
+        default:
+            return "Defaul del Reducer"
+    }
+}
