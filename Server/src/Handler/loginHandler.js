066f708dface4be6134f3768baad1b6df8402af6
KenMiyagi <kenrodrigoariel@gmail.com>
2023-08-29 17:34:46 -0300

    login/signUp-handlers/controllers
    Creé los handlers para el registro, e inicialicé los controlladores correspondientes que estos necesitan.
    A su vez necesito importar modelos, es por eso que haré un commit y un push para poder hacer pull de los modelos

---
+++ Server/src/Handler/loginHandler.js
@@ -0,0 +1,11 @@
+
+/* const loginHandler = async (req, res) =>{
+    try {
+        const {email, password} = req.body
+        //Debe consultar los 3 modelos
+        const adminAcc = await getAdminAccController(email)
+        if(adminAcc) 
+    } catch (error) {
+
+    }
+} */
