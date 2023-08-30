9c56c9ec3f8db602a3d871fcc21c9046ae8a9728
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2023-08-29 23:29:41 -0500

    Controller y handlers CRD modelo OFFER
    Controllers, handlers de offer
    POST, GET, DELETE

--- Server/src/Routes/index.js
+++ Server/src/Routes/index.js
@@ -2,10 +2,24 @@ const { Router } = require("express");
 const { trainingHandler } = require('../Handler/createTrainingHandler')
 const { getTrainingByName } = require('../Handler/getTrainingHandler')
 const { getAlltraining } = require('../Handler/getAllTraining')
-
+const {postOfferHandler, deleteOfferHandler, getOfferHandler} = require('../Handler/offersHandler')
+const { loginHandler } = require("../Handler/loginHandler");
+const { userCompaniesUpHandler } = require("../Handler/companiesHandlers");
+const { userSignUpHandler } = require("../Handler/userHandlers");
+const { adminSignUpHandler } = require("../Handler/adminHandlers");
 
 
 const router = Router();
+//Rutas de Offer
+router.post('/offer', postOfferHandler)
+router.delete('/offer', deleteOfferHandler)
+router.get('/offer', getOfferHandler)
+//-----------------------------------------------
+router.get("/login", loginHandler)
+router.post("/signup/company", userCompaniesUpHandler)
+router.post("/signup/user", userSignUpHandler)
+router.post("/signup/admin", adminSignUpHandler)
+//-----------------------------------------------
 
 router.get('/training', (req, res) => {
     const { name } = req.body;
