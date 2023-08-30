d0856398a2b2a72369ff57dce737e10fc5481ce6
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-08-30 15:13:02 +0200

    Instalacion y configuracion de TailwindCss
    Se ha instalado TailwindCss, se ha configurado para su funcionamiento y se ha agregado una paleta de colores custom. Tambien se ha implementado un boton en App para cambiar entre Dark mode y Light mode, esto habra que pasarlo a la NavBar en forma de switch.

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,11 +1,26 @@
 import "./App.css";
 import { Route, Routes, useLocation } from "react-router-dom";
 import Login from "./Views/Login";
-
+import { useState, useEffect } from "react";
+//****Todo lo relacionado con el tema del color debera pasarse a la NavBar****
 function App() {
+  const [theme, setTheme] = useState("light");
 
+  useEffect(() => {
+    if (theme === "dark"){
+      document.documentElement.classList.add("dark");
+    } else {
+      document.documentElement.classList.remove("dark");
+    }
+  }, [theme]);
+
+  const handleThemeSwitch = () => {
+    setTheme(theme === "dark" ? "light" : "dark");
+  }
+  
   return (
     <div>
+      <button class="text-white" onClick={handleThemeSwitch}>Dark Mode</button>
       <Routes>
         <Route path="/login" element={<Login />}/>
       </Routes>
