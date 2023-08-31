4f89a37acf0ddbbea25371f186b8c6d356dd76af
Elizabeth MIcaela Gurdzel <121364740+elizabethMica@users.noreply.github.com>
2023-08-30 21:39:18 -0300

    registerCompany update
    propiedades restantes agregadas. falta el estilo (y que sea responsive) (good luck mica del futuro)

--- Client/src/Views/RegisterCompany/validation.js
+++ Client/src/Views/RegisterCompany/validation.js
@@ -16,8 +16,8 @@ export default function validation(input){
         error.contraseñaRepetida = "Las contraseñas no coinciden."
     }
 
-    if(!input.nombre){
-        error.nombre = "Ingrese el nombre de la empresa."
+    if(!input.nombreEmpresa){
+        error.nombreEmpresa = "Ingrese el nombre de la empresa."
     }
 
     if(input.nombre.length > 20 || input.nombre.length < 5){
@@ -28,9 +28,35 @@ export default function validation(input){
         error.descripcion = "Queremos saber de qué trata la empresa."
     }
 
-    // if(input.descripcion.length < 5 ){
-    //     error.descripcion = "Cuentanos más!"
+    if(!input.nombre){
+        error.nombre ="Introduzca su nombre."
+    }
+
+    if(!input.apellido){
+        error.apellido ="Introduzca su apellido."
+    }
+
+    if(!input.cargo){
+        error.cargo ="Introduzca su cargo."
+    }
+
+    if(!input.telefono){
+        error.telefono ="Necesitamos un telefono para estar en contacto."
+    }
+
+    if(!input.capacitacion){
+        error.capacitacion = "Ingrese el rubro de la capacitación."
+    }
+
+    // if(typeof(input.telefono) !== Number){
+    //     error.telefono ="El numero de telefono debe ser valido"
     // }
+
+    if(input.paises === []){
+        error.paises ="Seleccione un pais."
+    }
+
+
 
     return error;
 }
