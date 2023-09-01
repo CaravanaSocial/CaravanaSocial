07b77d4dbb58c885caa9183366aeff5a653bd540
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-09-01 18:07:28 +0200

    Estilos RegisterUser

--- Client/src/Views/RegisterUser/RegisterUser.jsx
+++ Client/src/Views/RegisterUser/RegisterUser.jsx
@@ -1,5 +1,4 @@
 import { useState, useEffect } from "react";
-import styles from "./RegisterUser.module.css"
 import validation from "./validation";
 import { Link, useNavigate } from "react-router-dom";
 import { createUser, getCountry, getCity, getState } from "../../Redux/Actions/Actions";
@@ -108,114 +107,155 @@ export default function RegisterUser() {
 
 
     return (
-        <div className={styles.divReg} >
-            <h2>REGISTRO</h2>
-            <form className={styles.formReg} onSubmit={handleSubmit} >
-                <label>Nombre</label>
-                <input type='text' name='name' value={userData.name} onChange={handleChange} />
-                <p>{ errors.name ? errors.name : null }</p>
+        <div className="inline-block m-4 p-4">
+            <div className="border-spacing-96 border-2 border-zinc-100 dark:border-zinc-800 rounded-3xl p-4 my-4">
+                <h1 className="text-4xl border-b-2 border-zinc-100 dark:border-zinc-800">REGISTRO</h1>
+                <form onSubmit={handleSubmit} >
+                    <h2>Nombre</h2>
+                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        type='text'
+                        name='name'
+                        value={userData.name}
+                        onChange={handleChange} />
+                    <p className="text-red-600">{ errors.name ? errors.name : null }</p>
+                    
+                    <h2>Apellido</h2>
+                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        type='text'
+                        name='lastName'
+                        value={userData.lastName}
+                        onChange={handleChange} />
+                    <p className="text-red-600">{ errors.lastName ? errors.lastName : null }</p>
+                    
+                    <h2>Fecha de Nacimiento</h2>
+                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        type='date'
+                        name='birthdate'
+                        value={userData.birthdate}
+                        onChange={handleChange} />
+                    <p className="text-red-600">{ errors.birthdate ? errors.birthdate : null }</p>
+                    
+                    <h2>País</h2>
+                    <select className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        onChange={handleChange}
+                        name="country">
+                        <option value="default">Seleccioná un país...</option>
+                        {country.map((p)=> (
+                            <option key={p} value={p}>{p}</option>
+                        ))}
+                    </select>
+                    <p className="text-red-600">{ errors.country ? errors.country : null }</p>
 
-                <label>Apellido</label>
-                <input type='text' name='lastName' value={userData.lastName} onChange={handleChange} />
-                <p>{ errors.lastName ? errors.lastName : null }</p>
+                    <h2>Estado/Provincia</h2>
+                    <select className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        onChange={handleChange}
+                        name="state">
+                        <option value="default">Seleccioná un Estado/Provincia...</option>
+                        {state.stateName?.map((s)=> (
+                            <option key={s} value={s}>{s}</option>
+                        ))}
+                    </select>
+                    <p className="text-red-600">{ errors.state ? errors.state : null }</p>
+                        
+                    <h2>Ciudad</h2>
+                    <select className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        onChange={handleChange}
+                        name="city">
+                        <option value="default">Seleccioná una Ciudad...</option>
+                        {city.map((c)=> (
+                            <option key={c} value={c}>{c}</option>
+                        ))}
+                    </select>
+                    <p className="text-red-600">{ errors.city ? errors.city : null }</p>
 
-                <label>Fecha de Nacimiento</label>
-                <input type='date' name='birthdate' value={userData.birthdate} onChange={handleChange} />
-                <p>{ errors.birthdate ? errors.birthdate : null }</p>
+                    <h2>Tenes Certificado Único de Discapacidad (CUD)?</h2>
+                    <label> Si <input type='checkbox'
+                        checked={checkboxCUD === 'SI'}
+                        onChange={() => handleCheckboxCUDChange('SI')} /></label>
+                    <label> No <input type='checkbox'
+                        checked={checkboxCUD === 'NO'}
+                        onChange={() => handleCheckboxCUDChange('NO')} /></label>
 
-                <section>
-                <label>País </label>
-                <select onChange={handleChange} name="country">
-                    <option value="default">Seleccioná un país...</option>
-                    {country.map((p)=> (
-                        <option key={p} value={p}>{p}</option>
-                    ))}
-                </select>
-                <p>{ errors.country ? errors.country : null }</p>
-                </section>
-
-                <section>
-                <label>Estado/Provincia </label>
-                <select onChange={handleChange} name="state">
-                    <option value="default">Seleccioná un Estado/Provincia...</option>
-                    {state.stateName?.map((s)=> (
-                        <option key={s} value={s}>{s}</option>
-                    ))}
-                </select>
-                <p>{ errors.state ? errors.state : null }</p>
-                </section>
-
-                <section>
-                <label>Ciudad </label>
-                <select onChange={handleChange} name="city">
-                    <option value="default">Seleccioná una Ciudad...</option>
-                    {city.map((c)=> (
-                        <option key={c} value={c}>{c}</option>
-                    ))}
-                </select>
-                <p>{ errors.city ? errors.city : null }</p>
-                </section>
-
-                <label>Tenes Certificado Único de Discapacidad (CUD)?</label>
-                <label>SI <input type='checkbox'
-                    checked={checkboxCUD === 'SI'}
-                    onChange={() => handleCheckboxCUDChange('SI')} /></label>
-                <label>NO <input type='checkbox'
-                    checked={checkboxCUD === 'NO'}
-                    onChange={() => handleCheckboxCUDChange('NO')} /></label>
-
-                {checkboxCUD === "SI" ?
+                    {checkboxCUD === "SI" ?
                     <section>
-                        <label>Código CUD </label>
-                        <input type='text' name='CUD' value={userData.CUD} 
-                        onChange={handleChange} 
-                        placeholder="CODIGO CUD..." />
-                        <p>{ errors.CUD ? errors.CUD : null }</p>
+                        <h2>Código CUD</h2>
+                        <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                            type='text'
+                            name='CUD'
+                            value={userData.CUD}
+                            onChange={handleChange} 
+                            placeholder="CODIGO CUD..." />
+                        <p className="text-red-600">{ errors.CUD ? errors.CUD : null }</p>
                     </section> : null
-                }
+                    }
                 
-                <label>Email</label>
-                <input type='email' name='email' value={userData.email} onChange={handleChange} />
-                <p>{ errors.email ? errors.email : null }</p>
+                    <h2>Email</h2>
+                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        type='email'
+                        name='email'
+                        value={userData.email}
+                        onChange={handleChange} />
+                    <p className="text-red-600">{ errors.email ? errors.email : null }</p>
 
-                <label>Contraseña</label>
-                <input type='password' name='password' value={userData.password} onChange={handleChange} />
-                <p>{ errors.password ? errors.password : null }</p>
+                    <h2>Contraseña</h2>
+                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        type='password'
+                        name='password'
+                        value={userData.password}
+                        onChange={handleChange} />
+                    <p className="text-red-600">{ errors.password ? errors.password : null }</p>
 
-                <label>Tipo/s de Preferencia/s</label>
-                <select name="preferences">
-                    <option value="Default">Seleccionar...</option>
-                </select>
+                    <h2>Tipo/s de Preferencia/s</h2>
+                    <select className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        name="preferences">
+                        <option value="Default">Seleccionar...</option>
+                    </select>
 
-                <label>Certificados (Opcional)</label>
-                <input type='url' name='certificates' value={userData.certificates} onChange={handleChange} />
+                    <h2>Certificados (Opcional)</h2>
+                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                        type='url'
+                        name='certificates'
+                        value={userData.certificates}
+                        onChange={handleChange} />
 
-                <label>Sos Freelancer?</label>
-                <label>SI <input type='checkbox' checked={checkboxFreelancer === 'SI'}
-                    onChange={() => handleCheckboxFreeChange('SI')} />
-                </label>
-                <label>NO <input type='checkbox' checked={checkboxFreelancer === 'NO'}
-                    onChange={() => handleCheckboxFreeChange('NO')} />
-                </label>
+                    <h2>Sos Freelancer?</h2>
+                    <label> Si <input type='checkbox' checked={checkboxFreelancer === 'SI'}
+                        onChange={() => handleCheckboxFreeChange('SI')} />
+                    </label>
+                    <label> No <input type='checkbox' checked={checkboxFreelancer === 'NO'}
+                        onChange={() => handleCheckboxFreeChange('NO')} />
+                    </label>
 
-                {checkboxFreelancer === "SI" ? 
+                    {checkboxFreelancer === "SI" ? 
                     <section>
-                        <label>Descripción de tu Emprendimiento </label>
-                        <input type='text' name='description' value={userData.description} onChange={handleChange} />
-                        <p>{ errors.description ? errors.description : null }</p>
+                        <h2>Descripción de tu Emprendimiento</h2>
+                        <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                            type='text'
+                            name='description'
+                            value={userData.description}
+                            onChange={handleChange} />
+                        <p className="text-red-600">{ errors.description ? errors.description : null }</p>
         
-                        <label>Dirección de su negocio </label>
-                        <input type='text' name='adress' value={userData.adress} onChange={handleChange} />
-                        <p>{ errors.adress ? errors.adress : null }</p>
+                        <h2>Dirección de su negocio </h2>
+                        <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                            type='text'
+                            name='adress'
+                            value={userData.adress}
+                            onChange={handleChange} />
+                        <p className="text-red-600">{ errors.adress ? errors.adress : null }</p>
                     </section> : null
-                }
-                <br />
-                <button type="submit" >REGISTRARME</button>
-                <br />
-                <section>
-                    <Link to={"/register-company"} >Registrarme como Empresa</Link>
-                </section>
-            </form>
+                    }
+
+                    <hr/>
+
+                    <button className="bg-zinc-300 mt-2 text-black rounded-3xl p-2"
+                        type="submit" >REGISTRARME</button>
+                    <br />
+                    <Link to={"/register-company"} >
+                        <button className="bg-zinc-300 mt-2 text-black rounded-3xl p-2">Registrarme como Empresa</button>
+                    </Link>
+                </form>
+            </div>
         </div>
     )
 }
