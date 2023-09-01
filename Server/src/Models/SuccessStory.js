5f02232d3eff8c92ba6390bfea017af91fd9a548
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2023-09-01 17:13:13 -0500

    models
    Correccion de TEXT

--- Server/src/Models/SuccessStory.js
+++ Server/src/Models/SuccessStory.js
@@ -17,7 +17,7 @@ module.exports = (sequelize) => {
             allowNull: false,
         },
         testimony:{
-            type: DataTypes.STRING,
+            type: DataTypes.TEXT,
             allowNull: false
         }
     })
