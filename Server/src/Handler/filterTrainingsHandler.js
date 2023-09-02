750cb8c55f7464564aa4dbee896a9610335a4754
CamiloCabezas <116015687+CamiloCabezas@users.noreply.github.com>
2023-09-02 13:52:23 -0500

    filtro trainings
    Se hace el filtro completo de paises con rubros para los trainings

--- Server/src/Handler/filterTrainingsHandler.js
+++ Server/src/Handler/filterTrainingsHandler.js
@@ -1,7 +1,12 @@
 const { filterTrain } = require('../Controller/filterTrainings/filterTrain')
 
 const filterTrainings = async (req, res) => {
-    const leakedTrainings = await filterTrain(req.query);
+    try {
+        const leakedTrainings = await filterTrain(req.query);
+        return res.status(200).json(leakedTrainings)
+    } catch (error) {
+        return res.status(400).send("No se ha encontrado capacitaciones ")
+    }
 }
 
 module.exports = { 
