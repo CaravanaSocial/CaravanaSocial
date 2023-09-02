2ded1ba6b3a1b12094cd42142b415bbef0349286
KenMiyagi <kenrodrigoariel@gmail.com>
2023-09-02 13:46:09 -0300

    Include de categories para get companies
    Include de categories para get companies

--- Server/src/Routes/adminRoute.js
+++ Server/src/Routes/adminRoute.js
@@ -1,9 +1,9 @@
 const { Router } = require("express");
 const {adminSignUpHandler, getAdminsHandler, updateAdminHandler} = require('../Handler/adminHandlers')
-
+const {validateAdminAccount} = require("../Middlewares/validateAdminAccount")
 const router = Router()
 
-router.post("/signup", adminSignUpHandler);
+router.post("/signup",validateAdminAccount ,adminSignUpHandler);
 router.get("/all", getAdminsHandler);
 router.patch("/update/:id", updateAdminHandler);
 
