11d9da50af052450b8f56a934ebe5aafc1e7039a
KenMiyagi <kenrodrigoariel@gmail.com>
2023-09-02 20:31:08 -0300

    Ruta getOfferById
    Ruta getOfferById

--- Server/src/Controller/Offer/getOfferByIdController.js
+++ Server/src/Controller/Offer/getOfferByIdController.js
@@ -1,21 +1,37 @@
-const {offer, companies, areaTraining} = require("../../db")
+const { offer, companies, areaTraining } = require("../../db");
 
+const getOfferByIdController = async (id) => {
+  const foundOffer = await offer.findOne({
+    where: { id },
+    include: [
+      {
+        model: companies,
+        attributes: {
+          exclude: ["password"],
+        },
+        include: [
+          {
+            model: areaTraining,
+            attributes: ["name"],
 
-const getOfferByIdController = async (id) =>{
-    const foundOffer = await offer.findOne({
-        where:{id},
-        include: [{
-            model: companies,
-            indlude: [{
-                model: areaTraining
-            }]
-        }]
-    })
+            through: { attributes: [] },
+          },
+        ],
+      },
+      {
+        model: areaTraining,
+        attributes: ["name"],
+        through: { attributes: [] },
+      },
+    ],
+    attributes: ["title"],
+    through: { attributes: [] },
+  });
 
-    if(foundOffer) return foundOffer
-    throw Error("Offer not found")
-}
+  if (foundOffer) return foundOffer;
+  throw Error("Offer not found");
+};
 
-module.exports={
-    getOfferByIdController
-}
+module.exports = {
+  getOfferByIdController,
+};
