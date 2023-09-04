52578054902bdb15591aa302699dc13d9112dade
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2023-09-03 19:58:35 -0500

    Update getOfferController.js
    Debugged Filtro OfferController

--- Server/src/Controller/Offer/getOfferController.js
+++ Server/src/Controller/Offer/getOfferController.js
@@ -4,24 +4,29 @@ const { Op } = require("sequelize");
 const getOfferController = async (info) => {
   const { companyName, country, category } = info;
 
-  var filterOptions = []
+  var filterOptions = [];
 
-  if(companyName){
-    filterOptions.push({model:companies, where:{nameCompany:companyName}})
-  }else{
-    filterOptions.push({model:companies})
+  if (companyName) {
+    filterOptions.push({
+      model: companies,
+      where: { nameCompany: { [Op.iLike]: `%${companyName}%` } },
+    });
+  } else {
+    filterOptions.push({ model: companies });
   }
-  
+
   if (country) {
-    filterOptions.push({ model: companies, where: { 'location.contry': country } });
-  }
-  
-  if(category){
-    filterOptions.push({model:areaTraining, where:{name:category}})
-  }else{
-    filterOptions.push({model:areaTraining});
+    filterOptions.push({
+      model: companies,
+      where: { "location.country": country },
+    });
   }
 
+  if (category) {
+    filterOptions.push({ model: areaTraining, where: { name: category } });
+  } else {
+    filterOptions.push({ model: areaTraining });
+  }
 
   const offerFiltered = await offer.findAll({
     include: filterOptions,
@@ -29,10 +34,10 @@ const getOfferController = async (info) => {
 
   if (offerFiltered) {
     return offerFiltered;
-}else{
-  throw new Error('No hay avisos en la base de datos')
-}
-}
+  } else {
+    throw new Error("No hay avisos en la base de datos");
+  }
+};
 
 module.exports = {
   getOfferController,
