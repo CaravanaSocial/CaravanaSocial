16ee3d21165d7e15ea69f1810ed8c63394e2a7c7
KenMiyagi <kenrodrigoariel@gmail.com>
2023-09-04 20:58:04 -0300

    Google login
    Implementación de logueo para cuentas registradas con google.

--- Server/src/Handler/loginHandler.js
+++ Server/src/Handler/loginHandler.js
@@ -2,11 +2,39 @@ const { getAdminAccController } = require("../Controller/Admin/getAdminAccContro
 const { getCompanyAccController } = require("../Controller/Companies/getCompanyAccController")
 const { getUserAccController } = require("../Controller/User/getUserAccController")
 const {loginController} = require("../Controller/Login/loginController")
+const {loginGoogleController} = require("../Controller/Login/loginGoogleController")
 
 const loginHandler = async (req, res) =>{
-    const {email, password} = req.body
+    const {email, password, google} = req.body
     try {
         //Debe consultar los 3 modelo
+        if(google){
+            //-----------------------------------------------------------------
+            const adminAcc = await getAdminAccController(email)
+            if(adminAcc){
+                let accTokenType = await loginGoogleController(adminAcc)
+                if(accTokenType.acc.activate!==false){
+                    return res.status(200).json({...accTokenType, type:"admin"})
+                }return res.status(400).json({error: "Usuario bloqueado"})
+            }
+            //-----------------------------------------------------------------
+            const companyAcc = await getCompanyAccController(email)
+            if(companyAcc){
+                let accTokenType = await loginGoogleController(companyAcc)
+                if(accTokenType.acc.activate!==false){
+                    return res.status(200).json({...accTokenType, type:"company"})
+                }return res.status(400).json({error: "Usuario bloqueado"})
+            }
+            //-----------------------------------------------------------------
+            const userAcc = await getUserAccController(email)
+            if(userAcc){
+                let accTokenType = await loginGoogleController(userAcc)
+                if(accTokenType.acc.activate!==false){
+                    return res.status(200).json({...accTokenType, type:"user"})
+                }return res.status(400).json({error: "Usuario bloqueado"})
+            }
+        }
+
         const adminAcc = await getAdminAccController(email)
         if(adminAcc){
             let accTokenType = await loginController(adminAcc, password)
