6097962261a2f4214934a8ecdc8eb6fb67987255
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-09-05 18:00:00 -0500

    Console.log(depuring)

--- Server/src/Controller/Offer/postOfferController.js
+++ Server/src/Controller/Offer/postOfferController.js
@@ -1,29 +1,29 @@
 const { offer } = require("../../db");
-const {areaTraining} = require("../../db")
-const {Op} = require('sequelize')
+const { areaTraining } = require("../../db");
+const { Op } = require("sequelize");
 
 const postOfferController = async (info) => {
-  const {title, description, category} = info.data
-  const {id} = info;
-
-  console.log(id);
+  const { title, description, category } = info.data;
+  const { id } = info;
 
   if (title && description && id) {
     const [user, created] = await offer.findOrCreate({
       where: { title: title },
-      defaults: { title:title, description: description},
+      defaults: { title: title, description: description },
     });
     if (created) {
-      for(let i=0; i<category.length; i++){
-        const userCategory = await areaTraining.findOne({where:{name:category[i]}})
-        await user.addAreaTraining(userCategory)
+      for (let i = 0; i < category.length; i++) {
+        const userCategory = await areaTraining.findOne({
+          where: { name: category[i] },
+        });
+        await user.addAreaTraining(userCategory);
       }
-      await user.setCompany(id)
+      await user.setCompany(id);
       return user;
-    } else return created
+    } else return created;
   } else throw new Error("Info invalida o incorrecta");
 };
 
 module.exports = {
-    postOfferController
-}
+  postOfferController,
+};
