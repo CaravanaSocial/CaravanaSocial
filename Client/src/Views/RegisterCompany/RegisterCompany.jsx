8cfa2f0f0ecdaf182b915ba2aa472e63637951fe
Elizabeth MIcaela Gurdzel <121364740+elizabethMica@users.noreply.github.com>
2023-09-05 17:58:03 -0300

    profile and update company
    inicios del perfil (dashboard) de company y update listo y funcionando

--- Client/src/Views/RegisterCompany/RegisterCompany.jsx
+++ Client/src/Views/RegisterCompany/RegisterCompany.jsx
@@ -21,6 +21,7 @@ const RegisterCompany = () => {
   const category = useSelector((state) => state.categories);
   const globalErrors = useSelector((state) => state.errors);
 
+
   const [companyInput, setCompanyInput] = useState({
     name: "",
     lastName: "",
@@ -151,7 +152,6 @@ const RegisterCompany = () => {
         location: companyInput.location,
       })
     ).then((postError) => {
-      console.log(postError);
       if (!postError) {
         setCompanyInput({
           name: "",
