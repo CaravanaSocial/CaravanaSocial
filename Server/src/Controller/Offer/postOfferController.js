fbd1b82bebb505f829a3ba4f2d9fd510866b1632
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-09-05 21:28:29 -0500

    Notificacion de Email Terminada, Register user, register company, offer y trainings

--- Server/src/Controller/Offer/postOfferController.js
+++ Server/src/Controller/Offer/postOfferController.js
@@ -1,10 +1,29 @@
 const { offer } = require("../../db");
-const { areaTraining } = require("../../db");
+const { areaTraining, companies } = require("../../db");
 const { Op } = require("sequelize");
+const fs = require("fs");
+const path = require("path");
+const transporter = require("../../Tools/email");
+require("dotenv").config();
+const emailUser = process.env.EMAIL_USER;
+const emailTemplate = fs.readFileSync(
+  path.join(__dirname, "../../Templates/emailOffer.html"),
+  "utf-8"
+);
 
 const postOfferController = async (info) => {
   const { title, description, category } = info.data;
   const { id } = info;
+
+  const emailResponse = await companies.findOne({ where: { id: id } });
+  const email = emailResponse.dataValues.email;
+
+  const menssageRegister = {
+    from: emailUser,
+    to: email,
+    subject: "Creacion de oferta Laboral",
+    html: emailTemplate,
+  };
 
   if (title && description && id) {
     const [user, created] = await offer.findOrCreate({
@@ -19,6 +38,15 @@ const postOfferController = async (info) => {
         await user.addAreaTraining(userCategory);
       }
       await user.setCompany(id);
+
+      transporter.sendMail(menssageRegister, (error, info) => {
+        if (error) {
+          console.error("Error al enviar el correo electrónico :", error);
+        } else {
+          console.log("Correo electrónico enviado con éxito:", info.response);
+        }
+      });
+
       return user;
     } else return created;
   } else throw new Error("Info invalida o incorrecta");
--- Server/src/Controller/Trainings/getCreatedTraining.js
+++ Server/src/Controller/Trainings/getCreatedTraining.js
@@ -1,41 +1,68 @@
-const { where } = require('sequelize');
-const { training, companies, areaTraining } = require('../../db');
-
+const { where } = require("sequelize");
+const { training, companies, areaTraining } = require("../../db");
+const fs = require("fs");
+const path = require("path");
+const transporter = require("../../Tools/email");
+require("dotenv").config();
+const emailUser = process.env.EMAIL_USER;
+const emailTemplate = fs.readFileSync(
+  path.join(__dirname, "../../Templates/emailTraining.html"),
+  "utf-8"
+);
 
 const createdTrainingController = async (body, companyId) => {
-    try {
-        const {name, description, video, category} = body
-        const { id } = companyId
+  try {
+    const { name, description, video, category } = body;
+    const { id } = companyId;
 
-        const [user,created] = await training.findOrCreate({
-            where:{
-                video: video
-            },defaults:{
-                name,
-                description,
-                video
-            }
-        })
-        if(user){
-            for(let i = 0; i < category.length; i++){
-                const trainingCategory = await areaTraining.findOne({
-                    where:{
-                        name:category[i]
-                    }
-                })
-                await user.addAreaTraining(trainingCategory)
-            }
-        }
+    const emailResponse = await companies.findOne({ where: { id: id } });
+    const email = emailResponse.dataValues.email;
 
-        await user.setCompany(id)
+    const menssageRegister = {
+      from: emailUser,
+      to: email,
+      subject: "Creacion de Capacitacion con Exíto",
+      html: emailTemplate,
+    };
 
-        return user
-    } catch (error) {
-        console.log(error);
-        throw error
+    const [user, created] = await training.findOrCreate({
+      where: {
+        video: video,
+      },
+      defaults: {
+        name,
+        description,
+        video,
+      },
+    });
+    if (user) {
+      for (let i = 0; i < category.length; i++) {
+        const trainingCategory = await areaTraining.findOne({
+          where: {
+            name: category[i],
+          },
+        });
+        await user.addAreaTraining(trainingCategory);
+      }
     }
-}
+
+    await user.setCompany(id);
+
+    transporter.sendMail(menssageRegister, (error, info) => {
+      if (error) {
+        console.error("Error al enviar el correo electrónico :", error);
+      } else {
+        console.log("Correo electrónico enviado con éxito:", info.response);
+      }
+    });
+
+    return user;
+  } catch (error) {
+    console.log(error);
+    throw error;
+  }
+};
 
 module.exports = {
-    createdTrainingController
-}
+  createdTrainingController,
+};
