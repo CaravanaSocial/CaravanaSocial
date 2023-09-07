8accc3ecd6c9a0647ec20e744a425a24edc8991a
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-09-07 18:06:35 +0200

    Terminados estilos NavBar y Login

--- Client/src/Views/Login/Login.jsx
+++ Client/src/Views/Login/Login.jsx
@@ -41,25 +41,23 @@ export default function Login () {
     return (
         <div className="inline-block m-4 p-4">
             <section>
-                <h1 className="text-4xl border-b-2 border-zinc-100 dark:border-zinc-800 rounded-sm">Caravana Social</h1>
-                <p>Te invitamos a formar parte de la re-evolución inclusiva.</p>
+                <h1 className="text-4xl border-b-2 border-lime-600 dark:border-lime-700 rounded-sm dark:text-gray-300">Caravana Social</h1>
+                <p className="dark:text-gray-300">Te invitamos a formar parte de la re-evolución inclusiva.</p>
             </section>
 
             <section>
-                <div className="border-spacing-96 border-2 border-zinc-100 dark:border-zinc-800 rounded-3xl p-4 my-4">
-                    <h1 className="text-4xl border-b-2 border-zinc-100 dark:border-zinc-800">Inicio de Sesion</h1>
-                    <br />
-                    <p>{globalErrors.LOGIN?.error ? globalErrors.LOGIN.error : null}</p>
-                    <br />
-                    <input className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                <div className="justify-center border-spacing-96 border-2 border-lime-600 dark:border-lime-700 rounded-3xl p-4 my-4">
+                    <h1 className="text-3xl border-b-2 border-lime-600 dark:border-lime-700 dark:text-gray-300">Inicio de Sesión</h1>
+                    
+                    <input className="h-8 rounded-3xl px-2 my-2 bg-gray-100 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-600"
                         type="email"
                         name="email"
-                        placeholder="Email"
+                        placeholder="Correo electrónico"
                         onChange={handleChange}
                         value={userData.email}
                     />
                     <br />
-                    <input className="rounded-3xl px-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+                    <input className="h-8 rounded-3xl px-2 bg-gray-100 dark:bg-gray-800 text-gray-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-600"
                         type="password"
                         name="password"
                         placeholder="Contraseña"
@@ -67,45 +65,50 @@ export default function Login () {
                         value={userData.password}
                     />
                     <br />
-                    <button className="bg-zinc-300 mt-2 text-black rounded-3xl"
+                    {globalErrors.LOGIN?.error ? <text className="text-red-500">{globalErrors.LOGIN.error}</text> : null}
+                    <br />
+                    <button className="bg-gray-100 dark:bg-gray-800 rounded-3xl p-2 my-1 dark:text-gray-300"
                         onClick={handleSubmit}
                         type="submit"
-                    >Iniciar Sesion</button>
+                    >Iniciar Sesión</button>
                     <br />
-                    <GoogleLogin
-                        onSuccess={(CredentialResponse) => {
-                            const CredentialResponseDecoded = jwt_decode(CredentialResponse.credential)
-                            dispatch(login({
-                                email: CredentialResponseDecoded.email,
-                                google:true
-                            })).then((postError)=>{
-                                if(postError){
-                                    dispatch(
-                                        setNewErrors({
-                                            type: "LOGIN",
-                                            error: postError.response.data,
-                                          })
-                                    )
-                                }else{
-                                    navigate("/home")
-                                }
-                            })
-                        }}
-                        onError={()=>{
-                            console.log("LOGIN FAILED");
-                        }}
-                    />
+                    <div className="mt-1 w-[223px] inline-block">
+                        <GoogleLogin
+                            onSuccess={(CredentialResponse) => {
+                                const CredentialResponseDecoded = jwt_decode(CredentialResponse.credential)
+                                dispatch(login({
+                                    email: CredentialResponseDecoded.email,
+                                    google:true
+                                })).then((postError)=>{
+                                    if(postError){
+                                        dispatch(
+                                            setNewErrors({
+                                                type: "LOGIN",
+                                                error: postError.response.data,
+                                            })
+                                        )
+                                    }else{
+                                        navigate("/home")
+                                    }
+                                })
+                            }}
+                            onError={()=>{
+                                console.log("LOGIN FAILED");
+                            }}
+                        />
+                    </div>
+                    <br />
+                    
                     <Link to="/">
-                        <h4>He olvidado mi Contraseña</h4>
+                        <button className="bg-gray-200 dark:bg-gray-800 rounded-3xl p-2 my-2 dark:text-gray-300">Has olvidado tu contraseña?</button>
                     </Link>
-                    <hr />
-                    <h4>Aun no tienes cuenta?</h4>
-                    <h4>Registrate</h4>
+                    <h4 className="border-t-2 border-lime-600 dark:border-lime-700 dark:text-gray-200">Aun no tienes cuenta?</h4>
+                    <h4 className="dark:text-gray-200">Registrate</h4>
                     <Link to="/register-user">
-                        <button className="bg-zinc-300 mr-1 text-black rounded-3xl">Usuario</button>
+                        <button className="bg-gray-200 dark:bg-gray-800 rounded-3xl p-2 mr-1 mt-1 dark:text-gray-300">Usuario</button>
                     </Link>
                     <Link to="/register-company">
-                        <button className="bg-zinc-300 ml-1 text-black rounded-3xl">Empresa</button>
+                        <button className="bg-gray-200 dark:bg-gray-800 rounded-3xl p-2 ml-1 mt-1 dark:text-gray-300">Empresa</button>
                     </Link>
                 </div>
             </section>
