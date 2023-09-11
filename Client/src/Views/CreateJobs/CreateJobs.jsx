5f06a1cbcb10d267c1d01defeb1eebf7066273c7
Jefferson Baldion <jefferson.baldion.b@gmail.com>
2023-09-10 21:21:49 -0500

    Update OfferJobs

--- Client/src/Views/CreateJobs/CreateJobs.jsx
+++ Client/src/Views/CreateJobs/CreateJobs.jsx
@@ -16,6 +16,8 @@ export default function createJobs() {
     category: [],
   });
 
+  console.log(currentAccount)
+
   useEffect(() => {
     dispatch(getCategories());
   }, []);
@@ -78,89 +80,107 @@ export default function createJobs() {
     !inputJobs.title || !inputJobs.category || !inputJobs.description;
 
   return (
-    <div className=" h-full text-center ">
-      <div className="inline-block m-4 p-4 text-center ">
-        <div className="justify-center text-center border-2 border-lime-600 dark:border-lime-700 rounded-3xl p-4 m-4">
-          <h1 className="text-3xl mb-1 dark:text-gray-300">Crea un Aviso de Trabajo</h1>
+    <div className="bg-gray-100 p-4">
+      <div className="flex">
+        <div className="w-2/4 flex justify-end items-center">
+          <div className="justify-center text-center border-2 border-lime-600 dark:border-lime-700 rounded-3xl p-4 m-4">
+            <h1 className="text-4xl  border-b-2 border-zinc-100 dark:border-zinc-800">
+              Crea un Aviso de Trabajo
+            </h1>
 
-          <div className="border-t-2 border-lime-600 dark:border-lime-700"/>
+            <h2>Nombre del Aviso de Trabajo</h2>
+            <input
+              className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+              type="text"
+              name="title"
+              value={inputJobs.title}
+              onChange={handleChange}
+              placeholder="Nombre..."
+            />
+            <br />
+            {error.title && <span className="text-red-600">{error.title}</span>}
 
-          <h2 className="text-lg dark:text-gray-300">Nombre del Aviso de Trabajo</h2>
-          <input
-            className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-600"
-            type="text"
-            name="title"
-            value={inputJobs.title}
-            onChange={handleChange}
-            placeholder="Nombre..."
-          />
-          <br />
-          {error.title && <span className="text-red-600">{error.title}</span>}
-          <br />
-
-          <select
-            className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-600"
-            onChange={handleCategory}
-            name="category"
-          >
-            <option value="default">rubro</option>
-            {category?.map((c) => {
-              return (
-                <option key={c} value={c}>
-                  {c}
-                </option>
-              );
-            })}
-          </select>
-          <br />
-          {inputJobs.category.length ? (
-              <h2 className="text-lg dark:text-gray-300">Rubros seleccionados: </h2>
-            ) : null}
-              {inputJobs.category.map((cat, i) => {
+            <h2>Categoria</h2>
+            <select
+              className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+              onChange={handleCategory}
+              name="category"
+            >
+              <option value="default">rubro</option>
+              {category?.map((c) => {
                 return (
-                  <div key={i}>
-                    <button className="bg-gray-300 dark:bg-gray-800 rounded-3xl px-2 py-1 m-1 dark:text-gray-300 hover:bg-red-500 dark:hover:bg-red-500"
-                    onClick={handleDelCategory}
-                    value={cat}
-                    >{cat}
+                  <option key={c} value={c}>
+                    {c}
+                  </option>
+                );
+              })}
+            </select>
+            <br />
+            <span>Rubros seleccionados: </span>
+            <div className="p-2 m-auto bg-zinc-300 text-zinc-800 focus:border-transparent w-[200px] justify-center align-middle rounded-3xl">
+              {inputJobs.category.map((cat) => {
+                return (
+                  <div className="text-center bg-zinc-400 mb-1 rounded-3xl">
+                    {cat}
+                    <button
+                      className="bg-red-600 px-1 text-white h-[20px] m-auto rounded-3xl"
+                      onClick={handleDelCategory}
+                      value={cat}
+                    >
+                      {" "}
+                      x
                     </button>
                   </div>
                 );
               })}
-          <p
-            className="text-red-600"
-            style={{ visibility: error.category ? "visible" : "hidden" }}
-          >
-            {error.category}
-          </p>
+            </div>
+            <p
+              className="text-red-600"
+              style={{ visibility: error.category ? "visible" : "hidden" }}
+            >
+              {error.category}
+            </p>
 
-          <h2 className="text-lg dark:text-gray-300">Descripción</h2>
-          <textarea
-            className="rounded-3xl px-2 py-1 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-600"
-            type="text"
-            name="description"
-            cols="28"
-            rows="8"
-            value={inputJobs.description}
-            onChange={handleChange}
-            placeholder="Descripcion..."
-          />
-          <br />
-          {error.description && (
-            <span className="text-red-600">{error.description}</span>
-          )}
-          <br />
+            <h2>Descripción</h2>
+            <textarea
+              className="rounded-3xl px-2 py-1 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-lime-700"
+              type="text"
+              name="description"
+              cols="20"
+              rows="8"
+              value={inputJobs.description}
+              onChange={handleChange}
+              placeholder="Descripcion..."
+            />
+            <br />
+            {error.description && (
+              <span className="text-red-600">{error.description}</span>
+            )}
 
-          <button
-            className="bg-gray-300 dark:bg-gray-800 rounded-3xl p-2 my-2 dark:text-gray-300 border-2 border-transparent hover:border-lime-600 dark:hover:border-lime-700"
-            type="submit"
-            onClick={handleSubmit}
-            disabled={disabled}
-          >
-            Crear
-          </button>
+            <button
+              className="bg-zinc-300 mt-2 text-black rounded-3xl p-2"
+              type="submit"
+              onClick={handleSubmit}
+              disabled={disabled}
+            >
+              Crear
+            </button>
+          </div>
+        </div>
+        <div className="w-2/4 flex justify-center items-center">
+        <div className="w-2/4 flex flex-col justify-end items-center">
+              <h1 className="mb-4">PREVIEW</h1>
+              <div className="bg-white rounded-lg p-4 shadow-md text-center" style={{ width: '300px' }}>
+                  <h1 className="w-full h-48 object-cover rounded-t-lg">{inputJobs.title}</h1>
+                  <h2 className="text-2xl font-semibold mt-2">{inputJobs.title}</h2>
+                  <p className="text-gray-600">
+                  {inputJobs.description}
+                  </p>
+              </div>
+          </div>
         </div>
       </div>
     </div>
   );
 }
+
