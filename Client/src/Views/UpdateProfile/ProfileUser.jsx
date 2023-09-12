ae3e908f57b195c4eda4bebfa5cb3c0681015a55
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-09-12 17:52:56 +0200

    Estilos panel de Usuario

--- Client/src/Views/UpdateProfile/ProfileUser.jsx
+++ Client/src/Views/UpdateProfile/ProfileUser.jsx
@@ -47,8 +47,11 @@ export default function ProfileUser() {
   const [checkboxFreelancer, setCheckboxFreelancer] = useState(
     account.freelancer
   );
+  const [image, setImage] = useState(false);
 
-  console.log(trainingsUser);
+  const handleImage = () => {
+    setImage(image === true ? false : true);
+  };
 
   useEffect(() => {
     dispatch(getCountry());
@@ -188,78 +191,68 @@ export default function ProfileUser() {
 
   return (
     <div className="h-full">
-      <section className="flex ">
-        <div className="flex flex-col text-center border-spacing-96 border-2 border-light-1 dark:border-light-1 rounded-3xl p-4 m-4">
+      <div className="flex">
+        <div className="block text-center border-2 border-light-1 dark:border-light-1 rounded-3xl p-4 m-4">
           <img
             key={key}
-            className="h-[300px] w-[300px] rounded-full"
+            className="inline-block w-72 rounded-full border-2 border-light-1 dark:border-light-1 mb-2"
             src={profilePicture}
           />
+          <h2 className="font-topmodern">{account.name + " " + account.lastName}</h2>
 
-          <br />
-          <h1 className="font-vilaka text-[30px] font-bold">
-            {account.name + " " + account.lastName}
-          </h1>
-          <br />
-          {edit === false ? (
-            <button
-              className="font-topmodern border-2 border-light-1 rounded hover:text-light-1"
-              name="edit"
-              onClick={handleClick}
-            >
-              Editar Perfil
-            </button>
-          ) : (
-            <div className="flex flex-col">
-              <UploadImage />
+          {edit === true ? (
+            <div>
               <button
-                className="font-topmodern border-2 rounded border-light-1 bg-light-1 hover:text-white"
-                onClick={handleSaveImage}
+                className="bg-gray-300 dark:bg-gray-800 rounded-3xl p-2 dark:text-gray-300 mb-2 border-2 border-transparent hover:border-light-1 dark:hover:border-light-1"
+                onClick={handleImage}
               >
-                Guardar imagen
+                Editar imagen de perfil
               </button>
+
+              {image === true ? (
+                <div>
+                  <UploadImage />
+                  <button
+                    className="bg-gray-300 dark:bg-gray-800 rounded-3xl p-2 dark:text-gray-300 mb-2 border-2 border-transparent hover:border-light-1 dark:hover:border-light-1"
+                    onClick={handleSaveImage}
+                  >Guardar imagen</button>
+                </div>
+              ) : null}
+
+              <h2 className="font-topmodern">Nombre</h2>
               <input
-                className=" font-vilaka text-[22px] font-bold rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                 name="name"
                 onChange={handleChange}
                 value={dataAcc.name}
                 placeholder="Nombre..."
               />
-              {
-                <p className="text-red-600">
-                  {errors.name ? errors.name : null}
-                </p>
-              }
-              <br />
+              <p className="text-red-600">{errors.name ? errors.name : null}</p>
+
+              <h2 className="font-topmodern">Apellido</h2>
               <input
-                className=" font-vilaka text-[22px] font-bold rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                 name="lastName"
                 onChange={handleChange}
                 value={dataAcc.lastName}
                 placeholder="Apellido..."
               />
-              <p className="text-red-600">
-                {errors.lastName ? errors.lastName : null}
-              </p>
-              <br />
+              <p className="text-red-600">{errors.lastName ? errors.lastName : null}</p>
+
+              <h2 className="font-topmodern">Localizacion</h2>
               <select
-                className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                 name="country"
                 onChange={handleChange}
               >
                 <option>Pais</option>
                 {countries.map((c) => (
-                  <option key={c} value={c}>
-                    {c}
-                  </option>
+                  <option key={c} value={c}>{c}</option>
                 ))}
               </select>
-              <p className="text-red-600">
-                {errors.country ? errors.country : null}
-              </p>
-              <br />
+              <p className="text-red-600">{errors.country ? errors.country : null}</p>
               <select
-                className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                 name="state"
                 onChange={handleChange}
               >
@@ -270,82 +263,58 @@ export default function ProfileUser() {
                   </option>
                 ))}
               </select>
-              <p className="text-red-600">
-                {errors.state ? errors.state : null}
-              </p>
-              <br />
+              <p className="text-red-600">{errors.state ? errors.state : null}</p>
               <select
-                className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                 name="city"
                 onChange={handleChange}
               >
                 <option>Ciudad</option>
                 {cities.map((c) => (
-                  <option key={c} value={c}>
-                    {c}
-                  </option>
+                  <option key={c} value={c}>{c}</option>
                 ))}
               </select>
               <p className="text-red-600">{errors.city ? errors.city : null}</p>
-              <br />
-              <span className="font-topmodern">Ubicacion:</span>
-              <div className="p-2 m-auto bg-zinc-300 text-zinc-800 focus:border-transparent w-[200px] justify-center align-middle rounded-3xl">
-                <div className="text-center font-vilaka font-bold text-[25px] bg-zinc-400 mb-1 rounded-3xl">
-                  {dataAcc.location.country}
-                </div>
-                <div className="text-center font-vilaka font-bold text-[25px] bg-zinc-400 mb-1 rounded-3xl">
-                  {dataAcc.location.state}
-                </div>
-                <div className="text-center font-vilaka font-bold text-[25px] bg-zinc-400 mb-1 rounded-3xl">
-                  {dataAcc.location.city}
-                </div>
-              </div>
-              <br />
-              <input name="CUD" onChange={handleChange} placeholder="CUD..." />
+
+              <h2 className="font-topmodern">Codigo CUD</h2>
+              <input 
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                name="CUD"
+                onChange={handleChange}
+                placeholder="CUD..." />
               <br />
               <select
-                className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                 onChange={handleCategory}
                 name="category"
               >
                 <option value="default">Rubro</option>
-                {category?.map((c) => {
-                  return (
-                    <option key={c} value={c}>
-                      {c}
-                    </option>
-                  );
-                })}
+                  {category?.map((c) => {
+                    return (
+                      <option key={c} value={c}>{c}</option>
+                    );
+                  })}
               </select>
-              <br />
-              <span className="font-topmodern">Rubros seleccionados: </span>
-              <div className="p-2 m-auto bg-zinc-300 text-zinc-800 focus:border-transparent w-[200px] justify-center align-middle rounded-3xl">
-                {dataAcc.category?.map((cat) => {
-                  return (
-                    <div
-                      key={cat}
-                      className="text-center bg-zinc-400 mb-1 rounded-3xl"
-                    >
-                      {cat}
-                      <button
-                        className="bg-red-600 px-1 text-white h-[20px] m-auto rounded-3xl"
-                        onClick={handleDelCategory}
-                        value={cat}
-                      >
-                        {" "}
-                        x
-                      </button>
-                    </div>
-                  );
-                })}
-              </div>
-              <p
-                className="text-red-600"
+              {dataAcc.category.length ? (
+                <h2 className="text-lg dark:text-gray-300">
+                  Rubros seleccionados:{" "}
+                </h2>
+              ) : null}
+              {dataAcc.category?.map((cat) => {
+                return (
+                  <div key={cat}>
+                    <button
+                      className="bg-gray-300 dark:bg-gray-800 rounded-3xl px-2 py-1 m-1 dark:text-gray-300 hover:bg-red-500 dark:hover:bg-red-500"
+                      onClick={handleDelCategory}
+                      value={cat}
+                    >{cat} X</button>
+                  </div>
+                );
+              })}
+              <p className="text-red-600"
                 style={{ visibility: errors.category ? "visible" : "hidden" }}
-              >
-                {errors.category}
-              </p>
-              <br />
+              >{errors.category}</p>
+
               <h2 className="font-topmodern">Sos Freelancer?</h2>
               <label className="font-bold">
                 {" "}
@@ -365,89 +334,89 @@ export default function ProfileUser() {
                   onChange={() => handleCheckboxFreeChange(false)}
                 />
               </label>
+
               {checkboxFreelancer === true ? (
                 <section>
-                  <h2 className="font-topmodern">
-                    Descripción de tu Emprendimiento
-                  </h2>
-                  <input
-                    className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                  <h2 className="font-topmodern">Descripción de tu Emprendimiento</h2>
+                  <textarea
+                    className="rounded-3xl px-2 py-1 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                     type="text"
                     name="description"
                     placeholder="añadir descripcion"
                     value={dataAcc.description}
                     onChange={handleChange}
+                    cols="28"
+                    rows="8"
                   />
-                  <p className="text-red-600">
-                    {errors.description ? errors.description : null}
-                  </p>
+                  <p className="text-red-600">{errors.description ? errors.description : null}</p>
 
                   <h2 className="font-topmodern">Dirección de su negocio </h2>
                   <input
-                    className="rounded-3xl px-2 mb-2 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
+                    className="h-8 rounded-3xl px-2 my-2 bg-gray-300 dark:bg-gray-800 text-zinc-800 dark:text-gray-200 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
                     type="text"
                     name="address"
                     value={dataAcc.address}
                     onChange={handleChange}
                   />
-                  <p className="text-red-600">
-                    {errors.address ? errors.address : null}
-                  </p>
+                  <p className="text-red-600">{errors.address ? errors.address : null}</p>
                 </section>
               ) : null}
-              <br />
-              <section>
+                <section>
+                  <button
+                    className="bg-light-1 font-topmodern rounded-3xl p-2 mr-1 border-2 border-transparent dark:text-zinc-900 hover:text-white hover:scale-95"
+                    onClick={handleSubmit}
+                  >Guardar</button>
+                  <button
+                    className="bg-gray-300 dark:bg-gray-800 rounded-3xl p-2 dark:text-gray-300 ml-1 border-2 border-transparent hover:border-light-1 dark:hover:border-light-1"
+                    name="cancel"
+                    onClick={handleClick}
+                  >Cancelar</button>
+                </section>
+              </div>
+              ) : (
                 <button
-                  className="bg-light-1 font-topmodern mt-2 hover:text-white text-black rounded-3xl p-1"
-                  onClick={handleSubmit}
-                >
-                  Guardar
-                </button>
-                <button
-                  className="bg-zinc-300 font-topmodern hover:text-light-1 ml-2 mt-2 text-black rounded-3xl p-1"
-                  name="cancel"
+                  className="bg-light-1 font-topmodern rounded-3xl p-2 border-2 border-transparent dark:text-zinc-900 hover:text-white hover:scale-95"
+                  name="edit"
                   onClick={handleClick}
-                >
-                  Cancelar
-                </button>
-              </section>
-            </div>
-          )}
+                >Editar Perfil</button>
+              )}
         </div>
-        <div>
-          <h1 className="font-topmodern">
-            Capacitaciones en curso y completadas
-          </h1>
-          {trainingsUser.length > 0 ? (
-            <div className="flex flex-wrap">
-              {trainingsUser.map((t) => {
-                return (
-                  <div className="flex justify-center">
-                    <div className="text-center m-1 border-2 border-light-1 dark:border-light-1 hover:scale-95 p-4 rounded-3xl w-[300px]">
-                      <h2 className="text-xl border-b-2 border-light-1 dark:border-light-1 mb-2">
-                        {t.name}
-                      </h2>
-                      <div>
-                        <video
-                          className="inline-block border-2 border-light-1 dark:border-light-1 rounded-3xl"
-                          src={t.video[0]}
-                          controls
-                          width="200"
-                          height="150"
-                        />
-                      </div>
-                    </div>
-                  </div>
-                );
-              })}
-            </div>
-          ) : (
+
             <div>
-              <h1>Aún no te has inscripto a ni una capacitación</h1>
+              <h1 className="font-topmodern">
+              Capacitaciones en curso y completadas
+              </h1>
+
+              {trainingsUser.length > 0 ? (
+                <div className="flex flex-wrap">
+                  {trainingsUser.map((t) => {
+                    return (
+                      <div className="flex justify-center">
+                        <div className="text-center m-1 border-2 border-light-1 dark:border-light-1 hover:scale-95 p-4 rounded-3xl w-[300px]">
+                          <h2 className="text-xl border-b-2 border-light-1 dark:border-light-1 mb-2">
+                            {t.name}
+                          </h2>
+                          <div>
+                            <video
+                              className="inline-block border-2 border-light-1 dark:border-light-1 rounded-3xl"
+                              src={t.video[0]}
+                              controls
+                              width="200"
+                              height="150"
+                            />
+                          </div>
+                        </div>
+                      </div>
+                    );
+                  })}
+                </div>
+              ) : (
+                <div>
+                  <h1>Aún no te has inscripto a ni una capacitación</h1>
+                </div>
+              )}
             </div>
-          )}
-        </div>
-      </section>
+      </div>
     </div>
   );
 }
