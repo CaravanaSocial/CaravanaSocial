c329b4c70314da679f43a24351b9e36aa7655236
Alejandro Perez <ale.jacobmp@gmail.com>
2023-09-13 00:04:08 -0500

    bug controlado

--- Client/src/Views/Offers/Detail/DetailOffer.jsx
+++ Client/src/Views/Offers/Detail/DetailOffer.jsx
@@ -12,15 +12,13 @@ export default function DetailOffer() {
     dispatch(actions.getOfferByName(title));
   }, []);
 
-  const handleAppy = () => {
-
-  }
+  const handleAppy = () => {};
 
   const handleContact = () => {
     window.location.href = `mailto:${offer.company.email}`;
-  }
+  };
 
-  console.log(offer)
+  console.log(offer);
 
   return (
     <div className="bg-gray-100 h-full p-4">
@@ -37,17 +35,21 @@ export default function DetailOffer() {
                 {offer?.company?.nameCompany}
               </h1>
               <img
-                    src= {offer?.company?.profilePicture}
-                    alt="profile picture company"
-                    className="w-full h-48 object-cover rounded-t-lg"
-                />
+                src={offer?.company?.profilePicture}
+                alt="profile picture company"
+                className="w-full h-48 object-cover rounded-t-lg"
+              />
               <h2 className="text-2xl font-semibold mt-2">{offer?.title}</h2>
               <p className="text-gray-600">{offer?.description}</p>
             </div>
             <div className="mt-4 text-center">
-
-              {offer.areaTrainings.map(x=>{return <span className="inline-block bg-green-500 text-white px-2 py-1 m-2 rounded-full mr-2 text-center">{x.name}</span> })}
-
+              {offer?.areaTrainings?.map((x) => {
+                return (
+                  <span className="inline-block bg-green-500 text-white px-2 py-1 m-2 rounded-full mr-2 text-center">
+                    {x.name}
+                  </span>
+                );
+              })}
             </div>
           </div>
         </div>
@@ -57,7 +59,10 @@ export default function DetailOffer() {
             <button className="bg-blue-500 text-white px-4 py-2 rounded-lg mb-4 block">
               Aplicar
             </button>
-            <button onClick={handleContact} className="bg-green-500 text-white px-4 py-2 rounded-lg block">
+            <button
+              onClick={handleContact}
+              className="bg-green-500 text-white px-4 py-2 rounded-lg block"
+            >
               Contactar
             </button>
           </div>
