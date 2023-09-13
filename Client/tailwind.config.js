2e56294e269aa18955d3e7aaa6148f9496eafafe
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-09-13 23:06:12 +0200

    Menu se esconde al hacer click en el y toggle del tema recuerda su posicion.

--- Client/tailwind.config.js
+++ Client/tailwind.config.js
@@ -36,5 +36,8 @@ export default {
     },
   },
 
-  plugins: [require("tailwind-scrollbar-hide")],
+  plugins: [
+    require("tailwind-scrollbar-hide"),
+    require('tailwindcss-animated')
+  ],
 };
