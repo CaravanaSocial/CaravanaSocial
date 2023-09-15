1b1fe6cca2b0b75dc29b9581c65dd78337b12ce1
Elizabeth MIcaela Gurdzel <121364740+elizabethMica@users.noreply.github.com>
2023-09-14 21:21:51 -0300

    retoques de estilos
    offercard, trainingcar, home

--- Client/src/Views/Home/Home.jsx
+++ Client/src/Views/Home/Home.jsx
@@ -54,9 +54,7 @@ const Home = () => {
       name="asdasd"
       className="2xl:flex  2xl:flex-row xl:flex xl:flex-row  lg:flex lg:flex-col  text-center  flex-col flex  2xl:w-full 2xl:h-full"
     >
-      <div className=" border-r-2  2xl:w-[300px] xl:w-[300px] w-full">
-        <h1 className=" sm:h-[300px] h-[150px] w-[300px] "></h1>
-      </div>
+     
       <div className="  md:flex md:flex-col md:items-center w-full ">
         <section className=" flex flex-col items-center">
           <h1 className="font-vilaka font-bold text-[50px]">Freelancers: </h1>
@@ -68,7 +66,7 @@ const Home = () => {
         </section>
         <button
           onClick={() => handleFrelancer()}
-          className="font-topmodern border-2 my-5  hover:text-light-1  border-light-1 rounded p-1 "
+          className="font-topmodern border-2 my-5  hover:bg-light-1  border-light-1 rounded-3xl p-1 "
         >
           Ver Mas
         </button>
@@ -84,7 +82,7 @@ const Home = () => {
         </section>
         <button
           onClick={() => handleCap()}
-          className="font-topmodern border-2 my-5  hover:text-light-1 items-end  border-light-1 rounded p-1 "
+          className="font-topmodern border-2 my-5  hover:bg-light-1  border-light-1 rounded-3xl p-1"
         >
           Ver Mas
         </button>
@@ -98,7 +96,7 @@ const Home = () => {
         </section>
         <button
           onClick={() => handleOffer()}
-          className="font-topmodern border-2 my-5  hover:text-light-1  border-light-1 rounded p-1 "
+          className="font-topmodern border-2 my-5  hover:bg-light-1  border-light-1 rounded-3xl p-1 "
         >
           Ver Mas
         </button>
