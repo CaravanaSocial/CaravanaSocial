414bcdfe7dafa6a78650ed3bd3869c1fc2c8e6f6
CesarABaron <127266668+CesarABaron@users.noreply.github.com>
2023-09-15 20:03:01 -0500

    se agrego la alerta del inicio de sesion exitoso en componente login

--- Client/src/Views/Login/Login.jsx
+++ Client/src/Views/Login/Login.jsx
@@ -1,4 +1,4 @@
-import React, {useEffect, useRef} from "react";
+import React, { useEffect, useRef } from "react";
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { login, clearErrors, setNewErrors } from "../../Redux/Actions/Actions";
@@ -6,17 +6,17 @@ import { useDispatch, useSelector } from "react-redux";
 import validation from "./validation";
 import { GoogleLogin } from "@react-oauth/google";
 import jwt_decode from "jwt-decode";
-import Speaching from "../../components/Speaching"
+import Speaching from "../../components/Speaching";
+import Swal from "sweetalert2";
 
 export default function Login() {
+  useEffect(() => {
+    play("Inicia sesión");
+  }, []);
 
-  useEffect(()=>{
-    play("Inicia sesión")
-  },[])
-
-  const play = (text)=>{
-    speechSynthesis.speak( new SpeechSynthesisUtterance(text))
-  }
+  const play = (text) => {
+    speechSynthesis.speak(new SpeechSynthesisUtterance(text));
+  };
 
   const loginButton = useRef(null);
   const dispatch = useDispatch();
@@ -30,9 +30,9 @@ export default function Login() {
 
   const handleKeyPress = (event) => {
     if (event.key === "Enter") {
-        loginButton.current.click();
+      loginButton.current.click();
     }
-}
+  };
 
   const handleChange = (event) => {
     setUserData({
@@ -45,6 +45,15 @@ export default function Login() {
     event.preventDefault();
     dispatch(login(userData)).then((postError) => {
       if (!postError) {
+        Swal.fire({
+          title: "Inicio de sesión exitoso!",
+          text: "Bienvenido",
+          icon: "success",
+          customClass: {
+            popup: "inicioSesion",
+          },
+        });
+
         navigate("/home");
         dispatch(clearErrors());
       } else {
@@ -60,17 +69,29 @@ export default function Login() {
       {" "}
       <div className="inline-block m-4  p-4 h-screen">
         <section>
-          <h1 onClick={()=>play("Caravana Social")} name="title" className="text-4xl font-vilaka font-bold text-[50px] text-center  border-b-2 border-light-1 dark:border-light-1 rounded-sm dark:text-gray-300">
+          <h1
+            onClick={() => play("Caravana Social")}
+            name="title"
+            className="text-4xl font-vilaka font-bold text-[50px] text-center  border-b-2 border-light-1 dark:border-light-1 rounded-sm dark:text-gray-300"
+          >
             Caravana Social
           </h1>
-          <p onClick={()=>play("Te invitamos a formar parte de la re-evolución inclusiva.")} className=" font-nunito font-bold dark:font-medium text-[20px] dark:text-gray-300">
+          <p
+            onClick={() =>
+              play("Te invitamos a formar parte de la re-evolución inclusiva.")
+            }
+            className=" font-nunito font-bold dark:font-medium text-[20px] dark:text-gray-300"
+          >
             Te invitamos a formar parte de la re-evolución inclusiva.
           </p>
         </section>
 
         <section className="text-center items-center">
           <div className="justify-center border-spacing-96 border-2 border-light-1 dark:border-light-1 rounded-3xl p-4 my-4">
-            <h1 onClick={()=>play("Inicio de Sesión")}className="text-2xl font-nunito font-bold dark:font-medium text-[30px] text-center border-b-2 border-light-1 dark:border-light-1 dark:text-gray-300">
+            <h1
+              onClick={() => play("Inicio de Sesión")}
+              className="text-2xl font-nunito font-bold dark:font-medium text-[30px] text-center border-b-2 border-light-1 dark:border-light-1 dark:text-gray-300"
+            >
               Inicio de Sesión
             </h1>
 
@@ -103,7 +124,9 @@ export default function Login() {
               onClick={handleSubmit}
               type="submit"
               ref={loginButton}
-            >Iniciar Sesión</button>
+            >
+              Iniciar Sesión
+            </button>
             <br />
             <div className="mt-1 w-[223px] inline-block">
               <GoogleLogin
@@ -141,7 +164,10 @@ export default function Login() {
                 Has olvidado tu contraseña?
               </button>
             </Link> */}
-            <h4 onClick={()=>play("¿Aún no tienes cuenta? Registrate")} className="border-t-2 font-nunito font-bold dark:font-medium border-light-1 dark:border-light-1 dark:text-gray-200">
+            <h4
+              onClick={() => play("¿Aún no tienes cuenta? Registrate")}
+              className="border-t-2 font-nunito font-bold dark:font-medium border-light-1 dark:border-light-1 dark:text-gray-200"
+            >
               ¿Aún no tienes cuenta? Registrate
             </h4>
             {/* <h4 className="dark:text-gray-200 font-nunito  font-bold dark:font-medium">Registrate</h4> */}
