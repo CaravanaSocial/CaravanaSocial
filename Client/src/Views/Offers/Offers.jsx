f6b3bbaa8a2c6b233eb3e550353abff1836ee196
Andrei Faur <bogdan.andrei.faur@gmail.com>
2023-09-18 16:12:40 +0200

    Estilos Home-Offers

--- Client/src/Views/Offers/Offers.jsx
+++ Client/src/Views/Offers/Offers.jsx
@@ -44,10 +44,10 @@ export default function Offer() {
   };
 
   return (
-    <div className=" h-full  text-center m-4 p-4 flex flex-col ">
+    <div className="h-full text-center m-4 p-4 flex flex-col">
       {localStorage.length !== 0 ? (
         <>
-          <div className="border-2 text-center border-zinc-100 dark:border-zinc-800 rounded-3xl py-2 shadow-md">
+          <div className="border-2 justify-center text-center border-zinc-100 dark:border-zinc-800 rounded-3xl py-2 shadow-md">
             <select
               className="rounded-3xl px-2 mx-1 bg-zinc-300 text-zinc-800 focus:border-transparent focus:outline-none focus:ring-2 focus:ring-light-1"
               name="country"
@@ -85,8 +85,9 @@ export default function Offer() {
               ))}
             </select>
           </div>
+
           <h1 className="font-vilaka font-bold text-[50px]">Ofertas</h1>
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
+          <div className="flex flex-wrap justify-center text-center">
             {offers?.map((offer) => (
               <OfferCard key={offer.id} offer={offer} />
             ))}
